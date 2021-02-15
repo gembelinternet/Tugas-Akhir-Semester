879d913c6d09ccc9cabbfbc98ac77fa7f02784ee
G3MB3L <gembelinternet404@gmail.com>
2021-02-16 01:32:10 +0700

    done :kiss:

--- app.php
+++ app.php
@@ -1,4 +1,7 @@
 <?php
 header('content-type:application/json');
-echo json_encode(['x' => $_POST['nama_mahasiswa']]);
+
+$total = (($_POST['nilai_kehadiran']*20/100)+($_POST['nilai_tugas']*25/100)+($_POST['nilai_uts']*30/100)+($_POST['nilai_uas']*25/100));
+
+echo json_encode(['nama_mahasiswa' => $_POST['nama_mahasiswa'], 'total' => $total]);
 ?>
